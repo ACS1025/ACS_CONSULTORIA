fed8094a49477962b64a952a96e8d8c47d567cdf
Seu NomeACS1025 <seu.email@example.com>
2025-12-21 00:12:31 -0300

    up

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const projetos = document.querySelectorAll(".projeto");
-    const botao = document.querySelector('button');
+const toggle = document.getElementById('mobileMenuToggle');
+const mobileNav = document.getElementById('mobileNav');
 
-    botao.addEventListener('click', function() {
-        botao.textContent = 'Botão Clicado!';
-    });
+toggle.addEventListener('click', () => {
+  const isOpen = mobileNav.style.display === 'flex';
 
-    projetos.forEach(projeto => {
-        const informacoes = projeto.querySelector(".Informações do prjeto");
-
-        projeto.addEventListener("mouseover", function() {
-            informacoes.style.opacity = "0.7";
-            informacoes.style.backgroundColor = "#000";
-        });
-
-        projeto.addEventListener("mouseout", function() {
-            informacoes.style.opacity = "0";
-            informacoes.style.backgroundColor = "rgb(0, 100, 0)";
-        });
-    });
+  mobileNav.style.display = isOpen ? 'none' : 'flex';
+  mobileNav.setAttribute('aria-hidden', isOpen);
+  toggle.setAttribute('aria-expanded', !isOpen);
 });
